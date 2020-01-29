5f8e9a64918aa5d49cb8c0b864414a6ad932c8de
Rudy <rudy001@gannon.edu>
2020-01-29 17:18:31 -0500

    complete deck class structure

--- src/blackjack_02/Deck.java
+++ src/blackjack_02/Deck.java
@@ -11,4 +11,28 @@ package blackjack_02;
  */
 public class Deck {
     
+    private Card[] myCards = new Card[52];
+    private int nextCard = 0;
+    private String[] ranks = {};
+    private String[] suits = {};
+    
+    public Deck(){
+        initDeck();
+        shuffle();
+        
+    }
+
+    private void initDeck() {
+        
+    }
+
+    private void shuffle() {
+        
+    }
+    
+    public Card dealCard(){
+        
+        return null; //Don't forget to change this!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    }
+    
 }
