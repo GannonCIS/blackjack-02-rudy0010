6374306f5b444ad292897e197c8a31a8448f6e76
Jacob Rudy <rudy001@gannon.edu>
2020-02-05 15:54:54 -0500

    implemented shuffle in deck class

--- src/blackjack_02/Deck.java
+++ src/blackjack_02/Deck.java
@@ -34,7 +34,12 @@ public class Deck {
     }
 
     private void shuffle() {
-        
+        for(int i =0; i < myCards.length; i++){
+            Card temp = myCards[i];
+            int rand = (int)(Math.random()*52);
+            myCards[i] = myCards[rand];
+            myCards[rand] = temp;
+        }
     }
     
     public Card dealCard(){
